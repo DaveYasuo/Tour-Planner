ac93531237f226fad208a26ef80c2f057dc72424
Administrator <admin@example.com>
2022-03-02 20:06:27 +0100

    Added a common project solution, folder structure for big projects and big dreams

---
+++ Test.Tour-Planner.Mocks/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Test.Tour_Planner.Mocks
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
--- Tour-Planner/App.xaml.cs
+++ Tour-Planner/App.xaml.cs
@@ -13,5 +13,13 @@ namespace Tour_Planner
     /// </summary>
     public partial class App : Application
     {
+        /*
+         * We see that the Images and Resources folders reside in the startup project.
+         * While this is customary, they can technically reside in any project or even in their own project.
+         * However, I prefer to keep them in this project because it provides a marginal performance benefit.
+         * Typically, when using MVVM, the only other files in the startup project will be the MainWindow.xaml, App.xaml (and their constituent code behind files), and app.config files.
+         * The Images folder contains the images and icons that are displayed in the UI controls,
+         * whereas the Resources folder normally contains any resource files, such as XML schemas or text or data files that are used by the application.
+         */
     }
 }
