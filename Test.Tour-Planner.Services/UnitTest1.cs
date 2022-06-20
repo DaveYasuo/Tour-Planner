1f801ebc78302202f61a598ea3bb5aa7f5763fc0
smolasian <76787287+smolasian@users.noreply.github.com>
2022-06-20 20:46:51 +0200

    bit unit tests

--- Test.Tour-Planner.Services/UnitTest1.cs
+++ Test.Tour-Planner.Services/UnitTest1.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Tour_Planner.DataModels.Enums;
 using Tour_Planner.Models;
@@ -60,20 +61,57 @@ namespace Test.Tour_Planner.Services
             Tour tour = new Tour(title, origin, destination, description, type);
             IRestService service = new RestService();
             Tour result = await service.AddTour(tour);
-            result.Description = "Hiii";
-            result.Title = "Aloha";
+
             await service.UpdateTour(result);
             List <Tour> tourList = await service.GetTours();
-            foreach(Tour to in tourList)
+            result = tourList.Last();
+            result.Description = "Hiii";
+            result.Title = "Aloha";
+            foreach (Tour to in tourList)
             {
                 if(to.Id == result.Id)
                 {
+                    await service.DeleteTour(to.Id);
                     Assert.AreNotEqual(to.Description, description);
                     Assert.AreNotEqual(to.Title, title);
                     return;
                 }
             }
+            await service.DeleteTour(result.Id);
+
             Assert.Fail();
+        }
+
+        [Test]
+        //[TestCase("TestCase1", "Wien", "Linz", "ASDF", RouteType.fastest)]
+        [TestCase("TestCase1", "Graz", "Salzburg", "ASDF", RouteType.shortest)]
+        public async Task GetTours(string title, string origin, string destination, string description, RouteType type)
+        {
+            Tour tour = new Tour(title, origin, destination, description, type);
+            IRestService service = new RestService();
+            List<Tour> oldList =await service.GetTours();
+            Tour result = await service.AddTour(tour);
+            List<Tour> newList = await service.GetTours();
+            await service.DeleteTour(result.Id);
+            Assert.AreNotEqual(oldList, newList);    
+        }
+
+        [Test]
+        //[TestCase("TestCase1","Wien","Linz","ASDF",RouteType.fastest)]
+        [TestCase("TestCase1", "Graz", "Salzburg", "ASDF", RouteType.shortest)]
+        public async Task AddTourLogToDatabase(string title, string origin, string destination, string description, RouteType type)
+        {
+            Tour tour = new Tour(title, origin, destination, description, type); ;
+            IRestService service = new RestService();
+            Tour result = await service.AddTour(tour);
+            Assert.IsNotNull(result.Distance);
+            Assert.IsNotNull(result.ImagePath);
+            Assert.AreEqual(result.Title, title);
+            Assert.AreEqual(result.Origin, origin);
+            Assert.AreEqual(result.Destination, destination);
+            Assert.AreEqual(result.Description, description);
+            Assert.AreEqual(result.RouteType, type);
+            await service.DeleteTour(result.Id);
         }
     }
 }
