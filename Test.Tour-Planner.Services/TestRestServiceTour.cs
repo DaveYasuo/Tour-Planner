24ef463ab1dd482f971d9b47e8b857ba6b91f34f
Administrator <admin@example.com>
2022-06-21 01:46:56 +0200

    refactor tour unit tests

--- Test.Tour-Planner.Services/TestRestServiceTour.cs
+++ Test.Tour-Planner.Services/TestRestServiceTour.cs
@@ -106,114 +106,25 @@ namespace Test.Tour_Planner.Services
         {
             Tour tour = new Tour(title, origin, destination, description, type);
             IRestService service = new RestService();
-            List<Tour> oldList = await service.GetTours();
 
             Tour result = await service.AddTour(tour);
-
-            if (oldList != null)
+            List<Tour> list = await service.GetTours();
+            if (result != null)
             {
-                foreach (Tour to in oldList)
+                if (list != null)
                 {
-                    if (result != null && to.Id == result.Id)
+                    foreach (Tour to in list)
                     {
-                        Assert.AreEqual(to.Description, result.Description);
-                        Assert.AreEqual(to.Title, result.Title);
-                        return;
+                        if (to.Id == result.Id)
+                        {
+                            await service.DeleteTour(result.Id);
+                            Assert.Pass("Get Tour success");
+                            return;
+                        }
                     }
+                    Assert.Fail("Get Tour failed");
                 }
-
-                List<Tour> newList = await service.GetTours();
-
-                if (result != null) await service.DeleteTour(result.Id);
-                Assert.AreNotEqual(oldList, newList);
             }
-        }
-
-        [Test]
-        [TestCase("TestCase1", "Wien", "Linz", "ASDF", RouteType.fastest)]
-        [TestCase("TestCase1", "Graz", "Salzburg", "ASDF", RouteType.shortest)]
-        public async Task AddTourLogToDatabase(string title, string origin, string destination, string description, RouteType type)
-        {
-            Tour tour = new Tour(title, origin, destination, description, type);
-            IRestService service = new RestService();
-            Tour result = await service.AddTour(tour);
-            TimeSpan time = new TimeSpan(10000000);
-            TourLog tourLog = new TourLog(result.Id, DateTime.Now, time, Rating.medium, Difficulty.medium, 123456, "Comment");
-            tourLog = await service.AddTourLog(tourLog);
-            List<TourLog> tourLogs = await service.GetAllTourLogsFromTour(result);
-            await service.DeleteTour(result.Id);
-
-            Assert.AreEqual(tourLogs[0].Comment, tourLog.Comment);
-            Assert.AreEqual(tourLogs[0].Difficulty, tourLog.Difficulty);
-            Assert.AreEqual(tourLogs[0].Rating, tourLog.Rating);
-            Assert.AreEqual(tourLogs[0].TourId, tourLog.TourId);
-            Assert.AreEqual(tourLogs[0].Distance, tourLog.Distance);
-            Assert.AreEqual(tourLogs[0].Id, tourLog.Id);
-
-        }
-
-        [Test]
-        [TestCase("TestCase1", "Wien", "Linz", "ASDF", RouteType.fastest)]
-        [TestCase("TestCase1", "Graz", "Salzburg", "ASDF", RouteType.shortest)]
-        public async Task DeleteTourLogFromDatabase(string title, string origin, string destination, string description, RouteType type)
-        {
-            Tour tour = new Tour(title, origin, destination, description, type);
-            IRestService service = new RestService();
-            Tour result = await service.AddTour(tour);
-            TimeSpan time = new TimeSpan(10000000);
-            TourLog tourLog = new TourLog(result.Id, DateTime.Now, time, Rating.medium, Difficulty.medium, 123456, "Comment");
-            await service.AddTourLog(tourLog);
-            await service.DeleteTourLog(tourLog.Id);
-
-            List<TourLog> newTourLogs = await service.GetAllTourLogsFromTour(result);
-            await service.DeleteTour(result.Id);
-            if (newTourLogs.Last().Id == tourLog.Id) Assert.Fail();
-            if (newTourLogs.Count == 0)
-                Assert.Pass();
-        }
-
-        [Test]
-        [TestCase("TestCase1", "Wien", "Linz", "ASDF", RouteType.fastest)]
-        [TestCase("TestCase1", "Graz", "Salzburg", "ASDF", RouteType.shortest)]
-        public async Task UpdateTourLogFromDatabase(string title, string origin, string destination, string description, RouteType type)
-        {
-            Tour tour = new Tour(title, origin, destination, description, type);
-            IRestService service = new RestService();
-            Tour result = await service.AddTour(tour);
-            TimeSpan time = new TimeSpan(10000000);
-            TourLog tourLog = new TourLog(result.Id, DateTime.Now, time, Rating.medium, Difficulty.medium, 123456, "Comment");
-            tourLog = await service.AddTourLog(tourLog);
-
-            tourLog.Comment = "Comment2";
-            await service.UpdateTourLog(tourLog);
-            List<TourLog> tourLogs = await service.GetAllTourLogsFromTour(result);
-            await service.DeleteTour(result.Id);
-            if (tourLogs.Last().Id == tourLog.Id)
-            {
-                Assert.AreEqual(tourLogs.Last().Comment, tourLog.Comment); return;
-
-            }
-            Assert.Fail();
-
-        }
-
-        [Test]
-        [TestCase("TestCase1", "Wien", "Linz", "ASDF", RouteType.fastest)]
-        [TestCase("TestCase1", "Graz", "Salzburg", "ASDF", RouteType.shortest)]
-        public async Task GetTourLogsFromDatabase(string title, string origin, string destination, string description, RouteType type)
-        {
-            Tour tour = new Tour(title, origin, destination, description, type);
-            IRestService service = new RestService();
-            Tour result = await service.AddTour(tour);
-            TimeSpan time = new TimeSpan(10000000);
-            TourLog tourLog = new TourLog(result.Id, DateTime.Now, time, Rating.medium, Difficulty.medium, 123456, "Comment");
-            tourLog = await service.AddTourLog(tourLog);
-
-            List<TourLog> oldList = await service.GetAllTourLogs();
-            await service.DeleteTourLog(tourLog.Id);
-            List<TourLog> newList = await service.GetAllTourLogs();
-            await service.DeleteTour(result.Id);
-            Assert.AreNotEqual(oldList.Count, newList.Count);
         }
     }
 }
